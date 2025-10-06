d3361031bbce6113d66be04e98fdf49275bffaae
Khattiya <KhattiyaDCI072@gmail.com>
2025-10-07 06:01:11 +0700

    fix(resizeImage): change format file from jpeg to webp

--- src/utils/imageUtils.ts
+++ src/utils/imageUtils.ts
@@ -3,7 +3,7 @@ import sharp from 'sharp';
 export const resizeImage = async (buffer: Buffer, width = 800): Promise<Buffer> => {
     return await sharp(buffer)
         .resize({ width })
-        .toFormat('jpeg')
-        .jpeg({ quality: 80 })
+        .toFormat('webp')
+        .webp({ quality: 80 })
         .toBuffer();
 }
