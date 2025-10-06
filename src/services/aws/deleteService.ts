c0188446733ea3fb0c63cb36d7ae47589bd21f16
Khattiya <KhattiyaDCI072@gmail.com>
2025-10-07 03:26:03 +0700

    feat(aws): create delete file in S3 service

---
+++ src/services/aws/deleteService.ts
@@ -0,0 +1,18 @@
+import { s3 } from '../../config/s3Client';
+
+export const deleteFileFromS3 = async (fileUrl: string): Promise<void> => {
+
+    const bucketName = process.env.AWS_S3_BUCKET_NAME!;
+    const key = fileUrl.split('/').pop();
+
+    if (!key) {
+        throw new Error('Invalid file URL');
+    }
+
+    const params = {
+        Bucket: bucketName,
+        Key: key,
+    };
+
+    await s3.deleteObject(params).promise();
+}
