6d2973c46c21ec73ef8ddc6a235b4c843ab14e53
Khattiya <KhattiyaDCI072@gmail.com>
2025-10-27 17:13:06 +0700

    fix(cloudinary): add sub folder path

--- src/services/upload/deleteService.ts
+++ src/services/upload/deleteService.ts
@@ -1,10 +1,15 @@
 import { cloudinary } from '@config/upload/cloudinary';
-import type { UploadApiOptions, UploadApiResponse } from 'cloudinary';
 
 export const deleteCloudinaryByPublicId = async (publicId: string) => {
     try {
         const result = await cloudinary.uploader.destroy(publicId);
-        console.log('Cloudinary Deletion Result:', result);
+
+        if (result.deleted) {
+            console.log('Cloudinary Deletion Result:', result);
+        } else {
+            console.warn('Cloudinary Deletion Warning:', result);
+        }
+
     } catch (error) {
         console.error('Cloudinary Deletion Error:', error);
     }
--- src/services/upload/uploadService.ts
+++ src/services/upload/uploadService.ts
@@ -1,13 +1,17 @@
 import { cloudinary } from '@config/upload/cloudinary';
 import type { UploadApiOptions, UploadApiResponse } from 'cloudinary';
 
-const folder = process.env.CLOUDINARY_FOLDER || 'my-personal-web';
+const cloudinaryFolder = process.env.CLOUDINARY_FOLDER;
 
-export const uploadBufferToCloudinary = async (buffer: Buffer, options: UploadApiOptions = {}) => {
+export const uploadBufferToCloudinary = async (
+    buffer: Buffer, 
+    subFolder?: string, 
+    options: UploadApiOptions = {}
+) => {
     return new Promise<UploadApiResponse>((resolve, reject) => {
         const stream = cloudinary.uploader.upload_stream(
             { 
-                folder,
+                folder: subFolder ? `${cloudinaryFolder}/${subFolder}` : cloudinaryFolder,
                 resource_type: options.resource_type || 'image',
                 fetch_format: 'auto', 
                 quality: 'auto', 
