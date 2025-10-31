6906610fc3adf478b21ae2bb64da39973fa8e2ae
Khattiya <KhattiyaDCI072@gmail.com>
2025-10-31 14:36:16 +0700

    fix(logger): add skip paths into logger

--- src/config/log/logger.ts
+++ src/config/log/logger.ts
@@ -1,4 +1,11 @@
 import morgan from 'morgan';
 import { env } from '@config/env/env';
 
-export const logger = morgan(env.NODE_ENV === 'production' ? 'combined' : 'dev');
+const format = env.NODE_ENV === 'production' ? 'combined' : 'dev';
+const skipPaths = new Set(['/healthz', '/readyz']);
+
+export const logger = env.NODE_ENV === 'test'
+    ? (_req: any, _res: any, next: any) => next()
+    : morgan(format, {
+        skip: (req: any) => req.method === 'OPTIONS' || skipPaths.has(req.path),
+    });
