95b4ecfd3f53d333683a387c9cdb478e29357f69
Khattiya <KhattiyaDCI072@gmail.com>
2025-10-31 17:26:18 +0700

    fix(healthz): add no cach to healthz

--- src/controllers/performance/systemController.ts
+++ src/controllers/performance/systemController.ts
@@ -3,10 +3,11 @@ import { systemReady } from '@services/performance/systemService';
 
 // Health check controller
 export const healthz = (_req: Request, res: Response) => {
+    res.set('Cache-Control', 'no-store');
     res.status(200).json({ 
         status: 'ok',
-        updtime: Math.round(process.uptime()),
-        timeStamp: new Date().toISOString()
+        uptime: Math.round(process.uptime()),
+        timestamp: new Date().toISOString()
     });
 };
 
